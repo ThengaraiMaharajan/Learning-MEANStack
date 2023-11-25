850f16bfdc171e70f88fb87dd0c2c51cfe03042f
DESKTOP-HHQE7JL\theng <tmaharajan1999@gmail.com>
2023-11-25 17:23:25 +0530

    Site Framework, basic template components

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { SidebarComponent } from './components/template/sidebar/sidebar.component';
+import { FooterComponent } from './components/template/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
